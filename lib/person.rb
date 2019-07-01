ac1c7b69d6ae9d23e1e80f0879bc77ea2801d6c1
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 04:53:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -49,9 +49,7 @@ class Person
   end
 
   def take_bath
-    #hygiene = @hygiene + 4
-    hygiene = hygiene=(@hygiene + 4)
-
+    self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 end
