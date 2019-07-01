db2484a18639386a41ec7b7e0cdbf77e176f8706
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 04:22:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 class Person
 
   attr_reader :name
-  attr_accessor :bank_account
+  attr_accessor :bank_account, :happiness
 
   def initialize(name)
     @name = name
     @bank_account = 25
+    @happiness = 8
   end
 
 
