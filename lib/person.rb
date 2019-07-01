181d8452deef22d32a85eab92bf6d69ed35bfbd4
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 04:25:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,6 +7,12 @@ class Person
     @name = name
     @bank_account = 25
     @happiness = 8
+    if @happiness < 0
+      @happiness = 0
+    elsif @happiness > 10
+      @happiness = 10
+    end
+
   end
 
 
